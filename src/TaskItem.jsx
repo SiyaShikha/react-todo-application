e731ee08897055abf08c8920670851bc83804cdb
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-05-07 00:47:09 +0530

    removed checkbox from task item and made the strike on click of task item

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -3,12 +3,21 @@ import React from "react";
 export const TaskItem = ({ task, done, taskId, todoId, toggle }) => {
   return (
     <div>
-      <input
+      {
+        /* <input
         type="checkbox"
         checked={done}
         onChange={() => toggle(taskId, todoId)}
-      />
-      <span style={{ textDecoration: done ? "line-through" : "none" }}>
+      /> */
+      }
+      <span
+        style={{
+          textDecoration: done ? "line-through" : "none",
+          cursor: "pointer",
+        }}
+        role="button"
+        onClick={() => toggle(taskId, todoId)}
+      >
         {task}
       </span>
     </div>
